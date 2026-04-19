1950f7594fe334becb7582e3e75fce4275e4a465
Stride Not Strike <1714368965@qq.com>
2026-04-19 22:58:59 +0800

    Add files via upload

---
+++ src/config/profileConfig.ts
@@ -0,0 +1,43 @@
+import type { ProfileConfig } from "../types/config";
+
+export const profileConfig: ProfileConfig = {
+	// 头像
+	// 图片路径支持三种格式：
+	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
+	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
+	// 3. 远程 URL："https://example.com/avatar.jpg"
+	avatar: "assets/images/avatar.png",
+
+	// 名字
+	name: "飘云易旅",
+
+	// 个人签名
+	bio: "一个在数字世界中浪迹四方的凡人",
+
+	// 链接配置
+	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
+	// 访问https://icones.js.org/ 获取图标代码，
+	// 如果想使用尚未包含相应的图标集，则需要安装它
+	// `pnpm add @iconify-json/<icon-set-name>`
+	// showName: true 时显示图标和名称，false 时只显示图标
+	links: [
+		{
+			name: "Bilibili",
+			icon: "fa7-brands:bilibili",
+			url: "https://space.bilibili.com/515021432",
+			showName: false,
+		},
+		{
+			name: "GitHub",
+			icon: "fa7-brands:github",
+			url: "https://github.com/StrideNotStrike",
+			showName: false,
+		},
+		{
+			name: "Email",
+			icon: "fa7-solid:envelope",
+			url: "mailto:desmagmeta@163.com",
+			showName: false,
+		},
+	],
+};
